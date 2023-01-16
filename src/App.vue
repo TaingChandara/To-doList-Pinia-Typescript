b5c349e14a9e169a22ed7c28a50f98d2a85582d1
unknown <Chandara.Taing@jtrustroyal.com>
2023-01-16 15:21:33 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <todo-app/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TodoApp from "./components/TodoApp.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TodoApp,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
